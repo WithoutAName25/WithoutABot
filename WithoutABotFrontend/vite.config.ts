8a8a07c614053e63062d9a0b92bf9f8da3117e64
WithoutAName25 <72936889+WithoutAName25@users.noreply.github.com>
2021-11-13 14:24:38 +0100

    Restructured project

--- WithoutABotFrontend/vite.config.ts
+++ WithoutABotFrontend/vite.config.ts
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    emptyOutDir: true,
+    outDir: "../WithoutABotBackend/src/main/resources/frontend"
+  }
 })
