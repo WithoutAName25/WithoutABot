eb29b38c438f5e0b8cec2255da00479bde0ea75f
WithoutAName25 <72936889+WithoutAName25@users.noreply.github.com>
2021-11-13 15:26:30 +0100

    Add dependabot

--- WithoutABotBackend/build.gradle.kts
+++ WithoutABotBackend/build.gradle.kts
@@ -8,9 +8,14 @@ version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://m2.dv8tion.net/releases") }
+    maven { url = uri("https://repo.panda-lang.org/releases") }
 }
 
 dependencies {
+    implementation("net.dzikoysk:cdn-kt:1.11.6")
+    implementation("net.dv8tion:JDA:4.3.0_283")
+    
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
