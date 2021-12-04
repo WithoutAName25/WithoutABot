13e636132255e1f58d83f4a89a070ec86cf53b1b
WithoutAName25 <72936889+WithoutAName25@users.noreply.github.com>
2021-12-04 14:40:02 +0100

    Adding ktor

--- WithoutABotBackend/src/main/kotlin/eu/withoutaname/discordbots/withoutabot/web/api/WebApi.kt
+++ WithoutABotBackend/src/main/kotlin/eu/withoutaname/discordbots/withoutabot/web/api/WebApi.kt
@@ -1,7 +1,5 @@
 package eu.withoutaname.discordbots.withoutabot.web.api
 
-import io.ktor.application.*
-import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
@@ -11,9 +9,7 @@ object WebApi {
 	
 	private val server = embeddedServer(Netty, port = 8080) {
 		routing {
-			get("/") {
-				call.respondText("Hi, das ist eine krasse api")
-			}
+		
 		}
 	}
 	
