f1662f22473748a5fe1756c2cc59768fa6ff29d5
WithoutAName25 <withoutaname@withoutaname.eu>
2023-01-18 21:38:39 +0100

    Delete old frontend

--- frontend/src/lib/error.ts
+++
@@ -1,6 +0,0 @@
-export function getErrorResponse(e: unknown, errorStatusCode: number) {
-  return new Response(
-    e instanceof Error ? e.message : typeof e === "string" ? e : undefined,
-    { status: errorStatusCode }
-  )
-}
