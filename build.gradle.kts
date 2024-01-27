efd69edc9520097540ea2c2dd3afec1ceef3a0b0
WithoutAName25 <withoutaname@withoutaname.eu>
2024-01-28 00:10:15 +0100

    Add bot functionality and interaction DSL
    This commit adds a new Bot.kt file which contains the main function for bot operation. It also includes the implementation of DSL for interaction commands in InteractionDSL.kt. Additionally, the build.gradle.kts file has been updated to include Kord library for bot operation and enables the use of context-receivers feature in Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.9.21"
+    application
 }
 
 group = "eu.withoutaname.withoutabot"
 version = "0.1.0"
+
+application {
+    mainClass.set("eu.withoutaname.withoutabot.BotKt")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("dev.kord:kord-core:0.13.1")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        freeCompilerArgs = freeCompilerArgs + "-Xcontext-receivers"
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(17)
 }
